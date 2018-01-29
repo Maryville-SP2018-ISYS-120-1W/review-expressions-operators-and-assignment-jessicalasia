0cf4decd5bec8104c1acf65e0b0835a6a128f40c
jessica <jragland1@live.maryville.edu>
2018-01-28 22:41:42 -0500

    Review

---
+++ Contents.swift
@@ -0,0 +1,43 @@
+/*:
+ Jerald's dog, Anne, had 3 squeak toys. Yesterday, Anne recieved 2 more squeak toys.
+  Use constant originalToy and set it to equal 3, then add a constant newToy to equal the amount using compound. When finish, print.
+/*
+ 
+ //
+ 
+ 
+ 
+ 
+/*
+ 
+ 
+ /*:
+ I have 4 bags but want more, set a constant countOne that equals 4. I want to add 2 more for constant countTwo. Then, add 8 more for constant countThree. What is constant numberOfBags? When finish, print.
+/*
+ 
+ //
+ 
+ 
+ 
+ 
+/*
+ 
+ /*:Create a constant numberOfLoses of 50 and a doublee numberOfWins to 2.5. If it doesn't compile, convert where necessary.
+ /*
+ 
+ //
+ 
+ 
+ 
+ /*
+ 
+ /*:
+ Create a constant numberOfLoses equals a time b, but conver into Double.
+ /*
+ 
+ //
+ 
+ 
+ /*
+ 
+ 
